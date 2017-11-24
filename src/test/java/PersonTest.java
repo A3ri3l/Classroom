70334f6bd8fa11e467cff2cc7928104b187fb828
ersv1701 <erica.svensson@yh.nackademin.se>
2017-11-24 14:15:13 +0100

    added test for person

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -5,11 +5,31 @@ import org.junit.Test;
 import otherclasses.Person;
 
 public class PersonTest {
+	Person p = new Person("Erica", "Svensson", 32,'F');
 
 	@Test
-	public void test() {
-		Person p = new Person("Rafael", "Silva", 30,'M');
-		assertEquals(p.getFirstName().equals("Rafael"),true);
+	public void testSetAndGetFristName() {
+		p.setFirstName("Erica2");
+		assertEquals(p.getFirstName().equals("Erica2"),true);
 	}
+	
+	@Test
+	public void testSetAndGetLastNAme() {
+		p.setLastName("Svensson2");
+		assertEquals(p.getLastName().equals("Svensson2"),true);
+	}
+	
+	@Test
+	public void testSetAndGetAge() {
+		p.setAge(33);
+		assertEquals(p.getAge(),33);
+	}
+	
+	@Test
+	public void testSetAndGetGender() {
+		p.setGender('W');
+		assertEquals(p.getGender(), 'W');
+	}
+	
 
 }
